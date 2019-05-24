a9b06473009061d81da781159d54e67288b424e3
Francisco Olcina <francisco.olcina@alfresco.com>
2019-05-24 17:03:44 +0100

    improved shutdown-vms.sh

--- scripts/shutdown-vms.sh
+++ scripts/shutdown-vms.sh
@@ -8,4 +8,8 @@ RESOURCE_GROUP_NAME_DEPLOY="Alfresco_deployment"
 
 LIST_VMS=$($AZ_EXE vm list -g $RESOURCE_GROUP_NAME_DEPLOY --query "[].id" -o tsv)
 
-$AZ_EXE vm deallocate --ids $LIST_VMS --no-wait
+echo "Shutting down the following VMs:"
+echo ""
+$AZ_EXE vm list -g $RESOURCE_GROUP_NAME_DEPLOY --query "[].id"
+
+$AZ_EXE vm deallocate --ids $LIST_VMS --no-wait 1> /dev/null
